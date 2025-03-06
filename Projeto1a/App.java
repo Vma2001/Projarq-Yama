c689e4b86cdf215a4d0a96058f26f05180bcdc12
Vma2001 <141682916+Vma2001@users.noreply.github.com>
2025-03-06 20:17:03 -0300

    Delete Projeto1a directory

--- Projeto1a/App.java
+++
@@ -1,9 +0,0 @@
-package Projeto1a;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("\nHello!");
-        String version = System.getProperty("java.version");
-        System.out.println("Running Java Version "+version+"\n");
-    }
-}
